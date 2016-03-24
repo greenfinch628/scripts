77e61a61af92a636d5e9f5889000fd631c33c31f
“greenfinch628” <greenfinch628@gmail.com>
2016-03-24 15:47:30 -0500

    commit syncit.sh

---
+++ syncit.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+EXCLUDES=(
+ --exclude '\.*'
+ --exclude '*.vdi.0'
+ --exclude '*.vdi'
+ --exclude '*.iso'
+ --exclude '*.zip'
+ --exclude '*.box'
+ --exclude '*.rpm'
+ --exclude '*.tar'
+ --exclude '*.gz'
+ --exclude '*.gif'
+ --exclude '*.qcow2'
+ --exclude 'consul-data'
+ --exclude 'consul'
+)
+
+TS=$(date "+%Y%m%d%H%M%S")
+TS=$(date "+%m%d%H%M%S")
+
+OUTFILE=/home/rczys/Desktop/syncit-logs/${TS}-syncit.txt
+
+date > $OUTFILE
+echo >> $OUTFILE
+echo syncing Docker ------------------------------------------------------------------- >> $OUTFILE
+SOURCE=/home/rczys/Docker
+TARGET=/home/rczys/Dropbox/work/centos7-lab
+rsync -arvz --delete -a "${EXCLUDES[@]}" $SOURCE $TARGET >> $OUTFILE
+
+echo >> $OUTFILE
+echo syncing vagrant ------------------------------------------------------------------ >> $OUTFILE
+SOURCE=/home/rczys/vagrant
+TARGET=/home/rczys/Dropbox/work/centos7-lab
+rsync -arvz --delete -a "${EXCLUDES[@]}" $SOURCE $TARGET >> $OUTFILE
+
+echo >> $OUTFILE
+echo syncing Documents----------------------------------------------------------------- >> $OUTFILE
+SOURCE=/home/rczys/Documents
+TARGET=/home/rczys/Dropbox/work/centos7-lab
+rsync -arvz --delete -a "${EXCLUDES[@]}" $SOURCE $TARGET >> $OUTFILE
+
+
